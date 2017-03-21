6c57219cfe40c0500f0f396e2dac7dce7f3aa180
ffuis-aljosa <a.sljuka@ffuis.edu.ba>
2017-03-21 10:19:07 +0100

    Moving Tank

---
+++ src/tanks/ImageProvider.java
@@ -0,0 +1,12 @@
+package tanks;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class ImageProvider {
+    static Image sprites;
+    
+    public static void loadSprites() {
+        //sprites = ImageIcon(getResource("../images/sprites.png")).getImage();
+    }
+}
